53700833555b86830e215900088b45b104276ed9
cahhbwy <cahhbwy@mail.ustc.edu.cn>
2017-11-18 15:18:39 +0800

    模型增加bn层，指数衰减学习率

--- model_0.py
+++ model_0.py
@@ -6,12 +6,11 @@ import tensorflow as tf
 
 def model(x, y, batch_size, image_size):
     h_0 = tf.divide(tf.cast(x, tf.float32), 255.)
-    h_1 = lrelu(conv2d(h_0, 10, k_h=9, k_w=9, strides=(1, 1, 1, 1), name="conv2d_0"))
-    h_2 = lrelu(conv2d(h_1, 20, k_h=9, k_w=9, strides=(1, 1, 1, 1), name="conv2d_1"))
-    h_3 = lrelu(conv2d(h_2, 20, k_h=9, k_w=9, strides=(1, 1, 1, 1), name="conv2d_2"))
-    h_4 = lrelu(conv2d(h_3, 5, k_h=9, k_w=9, strides=(1, 1, 1, 1), name="conv2d_3"))
-    loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=h_4, labels=tf.one_hot(y, 5)))
-    predict = tf.cast(tf.argmax(h_4, axis=3), tf.uint8)
+    h_1 = batch_norm(conv2d(h_0, 10, k_h=11, k_w=11, strides=(1, 1, 1, 1), name="conv2d_0"), name="bn_1")
+    h_2 = tf.nn.relu(batch_norm(conv2d(h_1, 20, k_h=9, k_w=9, strides=(1, 1, 1, 1), name="conv2d_2"), name="bn_3"))
+    h_3 = conv2d(h_2, 5, k_h=7, k_w=7, strides=(1, 1, 1, 1), name="conv2d_4")
+    loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=h_3, labels=tf.one_hot(y, 5)))
+    predict = tf.cast(tf.argmax(h_3, axis=3), tf.uint8)
     acc = tf.reduce_mean(tf.cast(tf.equal(predict, y), tf.float32))
     return loss, acc, predict
 
@@ -21,13 +20,15 @@ if __name__ == '__main__':
     X, Y = read_data()
     X, Y = tf.train.shuffle_batch([X, Y], batch_size=batch_size, capacity=2000, min_after_dequeue=1000)
     m_loss, m_acc, m_predict = model(X, Y, batch_size=batch_size, image_size=256)
-    op = tf.train.AdamOptimizer(0.001).minimize(m_loss)
+    global_step = tf.Variable(0, trainable=False)
+    learning_rate = tf.train.exponential_decay(learning_rate=0.01, global_step=global_step, decay_steps=100, decay_rate=0.95)
+    op = tf.train.AdamOptimizer(learning_rate).minimize(m_loss)
     sess = tf.InteractiveSession()
     sess.run(tf.global_variables_initializer())
     tf.train.start_queue_runners(sess=sess)
     index = 0
     for s in range(10000):
-        _, v_loss = sess.run([op, m_loss])
+        _, v_loss = sess.run([op, m_loss], feed_dict={global_step: s})
         if s % 10 == 0:
             print("step %6d: loss = %f" % (s, v_loss))
         if s % 100 == 0:
